ce7dd7d31ce8cb56605138825581f1a94ff13005
xDrais <yassin001009@gmail.com>
2025-10-28 20:11:12 +0100

    Update app component and added home page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {MainPage} from '../core/main-page/main-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainPage, title: 'Home page'},
+];
