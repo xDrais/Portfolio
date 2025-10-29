48142d5d3487065a634048983fc129ffaf8bd62a
xDrais <yassin001009@gmail.com>
2025-10-29 18:09:52 +0100

    Added scene lights and orbit controles to interact with the scene

--- src/core/main-page/main-page.ts
+++ src/core/main-page/main-page.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component} from '@angular/core';
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 @Component({
   selector: 'app-main-page',
@@ -29,11 +30,40 @@ export class MainPage implements AfterViewInit {
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    camera.position.z = 10;
+    const geometryTwo = new THREE.TorusGeometry(10, 3, 16, 100);
+    const materialTwo = new THREE.MeshStandardMaterial({color: 0x00ff00});
+    const torus = new THREE.Mesh(geometryTwo, materialTwo);
+    scene.add(torus);
+
+
+    // point light
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(20, 20, 20);
+    scene.add(pointLight);
+
+    // ambient light
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(ambientLight);
+
+    // light helper to show the light positon
+    const lightHelper = new THREE.PointLightHelper(pointLight);
+    scene.add(lightHelper);
+
+    // grid helper for the pov
+    const gridHelper = new THREE.GridHelper(200, 5);
+    scene.add(gridHelper);
+
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    camera.position.z = 50;
 
     function animate() {
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      // requestAnimationFrame(animate);
+      torus.rotation.x += 0.01;
+      torus.rotation.y += 0.005;
+      torus.rotation.z += 0.01;
+
+      controls.update();
       renderer.render(scene, camera);
     }
   }
